0485e5c83142bbed63e4f8d6ae7f1e7dc5aaa0ba
James Shin <james.shin@hrsdc-rhdcc.gc.ca>
2017-10-06 13:42:32 -0400

    added simple templates for website

--- app.js
+++ app.js
@@ -10,6 +10,23 @@ var users = require('./routes/users');
 
 var app = express();
 
+//Mongoose Init
+var mongoose = require('mongoose');
+//Socket Io Init
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+console.log("Socket IO Server Connected");
+server.listen(80);
+
+//Mongoose DB connection
+mongoose.connect('mongodb://localhost/searchApp', { useMongoClient: true, promiseLibrary: global.Promise });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("MongoDB Connected");
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,6 +41,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+
+io.on('connection', function (socket) {
+
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
